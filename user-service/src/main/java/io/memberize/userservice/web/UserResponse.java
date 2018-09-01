31a2c1e21806e97fa8305226316d017e73d8725a
Atanas Minkov <naskospasko@gmail.com>
2018-09-02 01:47:55 +0300

    user service - initial work.

---
+++ user-service/src/main/java/io/memberize/userservice/web/UserResponse.java
@@ -0,0 +1,23 @@
+package io.memberize.userservice.web;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class UserResponse {
+    private String message;
+
+    public UserResponse() {
+    }
+
+    public UserResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
